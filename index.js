ddcf37996fd7d1e3cb6bf8e3d7e0be327ac8cd4f
jokay03J <magrojoan3@gmail.com>
2022-03-09 23:03:36 +0100

    add dependency and configurate package
    Dependency:
    -axios
    -jest
    
    Config Package:
    -version(0.0.1)
    -author
    -script test
    -description
    -name

---
+++ index.js
@@ -0,0 +1,9 @@
+const { DeezerGenerator } = require("./generator/deezer");
+const SpotifyGenerator = require("./generator/spotify");
+const YoutubeGenerator = require("./generator/youtube");
+
+module.exports = {
+  DeezerGenerator,
+  SpotifyGenerator,
+  YoutubeGenerator,
+}
