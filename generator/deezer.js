491eec5b33e132a659947402297847871e1182fa
jokay03J <magrojoan3@gmail.com>
2022-03-20 21:13:44 +0100

    update generator
    ADD:
    -SoundCloud
    
    REMOVE:
    -Deezer

--- generator/deezer.js
+++ generator/deezer.js
@@ -1,38 +1,38 @@
-const { DeezerURINotFound, DeezerGeneratorError, DeezerInvalidURI } = require("../errors/error");
+// const { DeezerURINotFound, DeezerGeneratorError, DeezerInvalidURI } = require("../errors/error");
 
-const axios = require("axios").default;
+// const axios = require("axios").default;
 
-class DeezerGenerator {
+// module.exports = class DeezerGenerator {
 
-  /**
-   * 
-   * @param {string} url 
-   * @returns {string} deezer widget url
-   */
-  static async embed(url) {
-    const encodedURL = encodeURI(url)
-    const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/oembed?url=${encodedURL}&maxwidth=700&maxheight=300&tracklist=true&format=json`, { headers: { origin: "*","x-requested-with": "axios/0.26"}})
-      .catch((err) => {
-        switch (err.response.status) {
-          case 404:
-            throw new DeezerURINotFound()
-            break;
+//   /**
+//    * 
+//    * @param {string} url
+//    * @returns {<Promise>string} deezer widget url
+//    */
+//   static async embed(url) {
+//     const promise = new Promise((resolve, reject) => {
+//       const encodedURL = encodeURI(url)
+//       axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/oembed?url=${encodedURL}&maxwidth=700&maxheight=300&tracklist=true&format=json`, { headers: { origin: "*", "x-requested-with": "axios/0.26" } })
+//         .then((res) => {
+//           return resolve(`https://widget.deezer.com/widget/auto/${res.data.entity}/${res.data.id}?autoplay=false&radius=true&tracklist=true`)
+//         })
+//         .catch((err) => {
+//           switch (err.response.status) {
+//             case 404:
+//               reject(new DeezerURINotFound())
+//               break;
 
-          case 400:
-            throw new DeezerInvalidURI()
-            break;
+//             case 400:
+//               reject(new DeezerInvalidURI())
+//               break;
 
-          default:
-            console.log(err);
-            throw new DeezerGenerator()
-            break;
-        }
-      })
-
-    return `https://widget.deezer.com/widget/auto/${res.data.entity}/${res.data.id}?autoplay=false&radius=true&tracklist=true`
-  }
-}
-
-module.exports = {
-  DeezerGenerator
-}
+//             default:
+//               console.log(err);
+//               reject(new DeezerGenerator())
+//               break;
+//           }
+//         })
+//     })
+//     return promise
+//   }
+// }
