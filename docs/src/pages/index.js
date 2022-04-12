003b23cfed8c705c65da09d7cf66cc11e40c96d7
jokay03J <jokay03j@gmail.com>
2022-04-12 19:42:49 +0200

    update main page and asset

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -7,18 +7,27 @@ import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          <div style={{ marginLeft: "2px", marginRight: "2px"}}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/getting-started">
+              Get Started
+            </Link>
+          </div>
+          <div style={{ marginLeft: "2px", marginRight: "2px"}}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              docs
+            </Link>
+          </div>
         </div>
       </div>
     </header>
@@ -26,10 +35,10 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`embed generator docs`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
