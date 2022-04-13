f964005b45543fd9912a37fe9c0d21453c8ff095
jokay03J <jokay03j@gmail.com>
2022-04-13 16:32:09 +0200

    update generator
    Modify:
    -YoutubeGenerator(add uri validation)

--- generator/youtube.js
+++ generator/youtube.js
@@ -1,4 +1,5 @@
-const { YoutubeInvalidURI } = require("../errors/error")
+const { YoutubeInvalidURI, YoutubeGeneratorError, YoutubeNotFound } = require("../errors/error")
+const axios = require("axios").default;
 
 module.exports = class YoutubeGenerator {
 
@@ -8,14 +9,31 @@ module.exports = class YoutubeGenerator {
    * @returns {<Promise>string} youtube video embed url
    */
   static async embed(url) {
-    const promise = new Promise((resolve,reject) => {
-      const videoId = new URL(url).searchParams.get("v")
+    const promise = new Promise((resolve, reject) => {
+      const encodedURL = encodeURI(url)
+      axios.get(`https://www.youtube.com/oembed?url=${encodedURL}&format=json`)
+        .then((res) => {
+          const html = res.data.html.split("src");
+          const result = html[1].split('"')[1]
+          return resolve(result)
+        })
+        .catch((err) => {
+          switch (err.response.status) {
+            case 400:
+              reject(new YoutubeNotFound())
+              break;
+            
+            case 404:
+              reject(new YoutubeInvalidURI())
+              break;
 
-      if(!videoId) reject(new YoutubeInvalidURI())
-  
-      resolve(`https://youtube.com/embed/${videoId}`)
-    })
-
-    return promise
+            default:
+              console.log(err);
+              reject(new YoutubeGeneratorError())
+              break;
+          }
+        })
+    });
+    return promise;
   }
 }
