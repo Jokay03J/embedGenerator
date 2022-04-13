1afd2728a126bf0990647db0a27e90673dee443f
jokay03J <jokay03j@gmail.com>
2022-04-13 15:24:00 +0200

    update config for support blog and docs edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/Jokay03J/embedGenerator/tree/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Jokay03J/embedGenerator/tree/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
