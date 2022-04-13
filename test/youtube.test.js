d5ad7bdc673cb42b2a8489b78c2bb90bf5127097
jokay03J <jokay03j@gmail.com>
2022-04-13 16:32:50 +0200

    modify test
    -youtube generator test(youtube.test.js)

--- test/youtube.test.js
+++ test/youtube.test.js
@@ -1,5 +1,5 @@
 const { YoutubeGenerator } = require("../index")
 
-test("https://www.youtube.com/watch?v=SeMNJlfBJdE to https://youtube.com/embed/SeMNJlfBJdE",async () => {
-  expect(await YoutubeGenerator.embed("https://www.youtube.com/watch?v=SeMNJlfBJdE")).toBe("https://youtube.com/embed/SeMNJlfBJdE")
+test("https://www.youtube.com/watch?v=SeMNJlfBJdE to https://www.youtube.com/embed/SeMNJlfBJdE?feature=oembed",async () => {
+  expect(await YoutubeGenerator.embed("https://www.youtube.com/watch?v=SeMNJlfBJdE")).toBe("https://www.youtube.com/embed/SeMNJlfBJdE?feature=oembed")
 })
