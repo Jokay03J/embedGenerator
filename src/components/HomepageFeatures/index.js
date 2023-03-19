8a758696d36d39170674c2e99789e85929023019
jokay03J <magrojoan3@gmail.com>
2023-03-19 02:08:00 +0100

    update home page & update FeatureList components

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,43 +1,39 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Png: require("@site/static/img/easyToUse.png").default,
     description: (
       <>
-        embed generator was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Embed generator was designed from the ground up to be easily installed
+        and used to get your website up and running quickly.
       </>
     ),
   },
   {
-    title: 'Fast',
-    Png: require('@site/static/img/fastDelivrery.png').default,
-    description: (
-      <>
-        embed generator is fast.
-      </>
-    ),
+    title: "Fast",
+    Png: require("@site/static/img/fastDelivrery.png").default,
+    description: <>Embed generator is fast.</>,
   },
   {
-    title: 'Promise',
-    Png: require('@site/static/img/promiseSupport.png').default,
-    description: (
-      <>
-        Promise is fully supported !
-      </>
-    ),
+    title: "Typescript",
+    Png: require("@site/static/img/promiseSupport.png").default,
+    description: <>Written in typescript for type checking and avoid bugs.</>,
   },
 ];
 
-function Feature({Svg,Png, title, description}) {
+function Feature({ Svg, Png, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
-        {Svg ?<Svg className={styles.featureSvg} role="img" /> : <img src={Png} className={styles.featureSvg}/>}
+        {Svg ? (
+          <Svg className={styles.featureSvg} role="img" />
+        ) : (
+          <img src={Png} className={styles.featureSvg} />
+        )}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <div style={{ marginLeft: "2px", marginRight: "2px"}}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/getting-started">
-              Get Started
-            </Link>
-          </div>
-          <div style={{ marginLeft: "2px", marginRight: "2px"}}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/intro">
-              docs
-            </Link>
-          </div>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Introduction
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/getting-started"
+          >
+            Getting Started
+          </Link>
         </div>
       </div>
     </header>
@@ -38,8 +37,9 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`embed generator docs`}
-      description="Description will go into a meta tag in <head />">
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
