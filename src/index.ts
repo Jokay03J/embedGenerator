44b12dfec9016be7dac290111839f95357cbec3c
jokay03J <magrojoan3@gmail.com>
2023-08-06 15:31:28 +0200

    move all errors and generators into src

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// generators
+export * from "./generators/deezer";
+export * from "./generators/souncloud";
+export * from "./generators/spotify";
+export * from "./generators/youtube";
+
+// errors
+export * from "./errors/error";
